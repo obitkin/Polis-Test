606e6cdbf135e158c39d2daa67f4232026c3c015
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-01 20:27:32 +0300

    Add message button

--- src/main/java/UserCard.java
+++ src/main/java/UserCard.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class UserCard {
 
     WebElement root;
@@ -13,5 +15,9 @@ public class UserCard {
 
     public String getName() {
         return root.findElement(By.xpath(".//div[@class='ellip']")).getText();
+    }
+
+    public List<WebElement> getMessageButton() {
+        return root.findElements(By.xpath(".//div[@class='tico']"));
     }
 }
