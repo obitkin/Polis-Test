e4907bd2c361258ff297459a2d3fc85dac8ac5ed
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-01 20:16:37 +0300

    Add names guest check

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,13 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.WebElement;
 
-import static com.codeborne.selenide.Selenide.$;
+import java.util.List;
 
-public class Test extends BaseTest{
+public class Test extends BaseTest implements TestData {
 
     LoginPage loginPage;
-
-    String login = "edr";
-    String password = "erftry";
 
     @Before
     public void begin() {
@@ -18,8 +17,19 @@ public class Test extends BaseTest{
     }
 
     @org.junit.Test
-    public void loginTest() {
-        loginPage.loginMe(login, password);
+    public void MyTest() {
+        UserPage userPage = loginPage.loginMe(login, password);
+        Assert.assertEquals(userPage.getCurrentUrl(), userPageURL);
+
+        GuestPage guestPage = userPage.goToGuest();
+        List<UserCard> listOfUsers = guestPage.getGuestCard();
+
+        Assert.assertEquals(names.size(), listOfUsers.size());
+        listOfUsers.forEach(x -> System.out.println(x.getName()));
+
+        for (UserCard userCard : listOfUsers) {
+            Assert.assertTrue(names.contains(userCard.getName()));
+        }
     }
 
     @After
