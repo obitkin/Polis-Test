82ce03ae487d88c817b78be5a8bac686e73c9cfb
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-09 00:05:43 +0300

    Add data file

---
+++ src/test/java/TestData.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface TestData {
+
+    String login = "";
+    String password = "";
+    String userPageURL = "https://ok.ru/";
+    List<String> namesFromTest = List.of("Имя гостя 1" , "Имя гостя 2");
+}
