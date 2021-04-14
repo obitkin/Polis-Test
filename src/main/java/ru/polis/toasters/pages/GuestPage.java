1f295100bf8de77377d00d79fe80681c03ad1c60
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-14 16:52:37 +0300

    Refactor by adding toolbars and guestblock

--- src/main/java/ru/polis/toasters/pages/GuestPage.java
+++ src/main/java/ru/polis/toasters/pages/GuestPage.java
@@ -1,28 +1,32 @@
 package ru.polis.toasters.pages;
 
 import com.codeborne.selenide.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$$;
+import ru.polis.toasters.elements.GuestBlock;
+import ru.polis.toasters.elements.Toolbar;
+import ru.polis.toasters.elements.ToolbarRight;
 
 public class GuestPage {
 
-    private final ElementsCollection guestCards = $$(byXpath("//div[@class=\"portlet_b\"]/.//div[@class=\"user-grid-card\"]"));
+    private final Toolbar toolbar = new Toolbar();
 
-    public List<UserCard> getGuestCard() {
-        List<UserCard> res = new ArrayList<>();
-        /*
-         * Нижная строка пресдтавляет собой выход логики теста в класс POM
-         * Эту архитектуру можно улучшить
-         * Вроде предлагалось в ru.polis.toasters.pages.UserCard добавить статический метод получения листа карт гостей
-         * И там сделать проверку на появление этих карт/страницы гостей
-         */
-        guestCards.shouldBe(CollectionCondition.sizeGreaterThan(1));
-        for (SelenideElement s : guestCards) {
-            res.add(new UserCard(s));
-        }
-        return res;
+    public Toolbar getToolbars() {
+        return toolbar;
     }
+
+    private final GuestBlock guestBlock = new GuestBlock();
+
+    public GuestBlock getGuestBlock() {
+        return guestBlock;
+    }
+
+    private final ToolbarRight toolbarRight = new ToolbarRight();
+
+    public ToolbarRight getToolbarRight() {
+        return toolbarRight;
+    }
+
+    public GuestPage() {
+        guestBlock.getSelenideBlock().shouldBe(Condition.visible);
+    }
+
 }
