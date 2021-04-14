3c6a8f5d89a89e5099900733117b6b9172cfb22a
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-14 20:07:30 +0300

    Add test data

--- src/test/java/ru/polis/toasters/data/TestData.java
+++ src/test/java/ru/polis/toasters/data/TestData.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public interface TestData {
 
-    String login = "";
-    String password = "";
-    String userPageURL = "https://ok.ru/";
-    List<String> namesFromTest = List.of("Имя гостя 1" , "Имя гостя 2");
+    // Данные бота 1
+    String user1 = "89995632015";
+    String userName1 = "Карл Дёниц";
+    String password1 = "Kirill11";
+    String url1 = "https://ok.ru/profile/597561870126";
+
+    // Данные бота 2
+    String user2 = "89944056284";
+    String userName2 = "Георгий Второй";
+    String password2 = "Kirill2";
+    String url2 = "https://ok.ru/profile/586820330668";
+
 }
