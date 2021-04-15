b8c35e838f1920d8b0c5e71e93304ff855f41324
Robert Andreev <andreev.robert.a@gmail.com>
2021-04-15 20:07:38 +0300

    Add UserData

---
+++ src/test/java/ru/polis/toasters/util/UserData.java
@@ -0,0 +1,16 @@
+package ru.polis.toasters.util;
+
+public class UserData {
+
+    public UserData(String user, String userName, String password, String url) {
+        this.user = user;
+        this.userName = userName;
+        this.password = password;
+        this.url = url;
+    }
+
+    public String user;
+    public String userName;
+    public String password;
+    public String url;
+}
